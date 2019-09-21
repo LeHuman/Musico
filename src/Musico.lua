94d2e5169032814ba02ee85e631f6e3317d597fc
LeHuman <47549331+LeHuman@users.noreply.github.com>
2019-09-21 15:28:01 -0500

    inital files

---
+++ src/Musico.lua
@@ -0,0 +1,12 @@
+-- Musico v0.1
+--[[
+    An adaptive music manager that uses songs that are split
+    into tracks in order to create horizontal dynamic
+    music
+]]
+
+local pieces = {}
+
+local function newPiece()
+
+end
---
+++ src/main.lua
@@ -0,0 +1,23 @@
+--Love2D Test of Musico
+
+function love.keypressed(key)
+end
+
+function love.mousepressed(x, y, button)
+end
+
+function love.mousereleased(_, _, button)
+end
+
+function love.mousemoved(x, y)
+end
+
+function love.load()
+
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+end
